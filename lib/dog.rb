3a7595af69ab5f1505ce21492b17415403d91b12
SaturdayAM <jwl62@georgetown.edu>
2018-03-14 09:49:30 -0400

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,18 @@
+class Dog
+	def name
+		@name
+	end
+
+	def name=(to_name)
+		@name = to_name
+	end
+
+	def breed
+		@breed
+	end
+
+	def breed=(to_set)
+		@breed = to_set
+	end
+
+end
---
+++ lib/person.rb
@@ -0,0 +1,17 @@
+class Person
+	def name
+		@name
+	end
+
+	def name=(to_name)
+		@name = to_name
+	end
+
+	def job
+		@job
+	end
+
+	def job=(to_set)
+		@job = to_set
+	end
+end
